75b3711ddeed6a78a043978ff668c2c200937a9d
Yimin2 <gownfla14@naver.com>
2024-07-10 13:25:44 +0900

    first commit

---
+++ src/main/java/com/ll/hackathon1team/Hackathon1TeamApplication.java
@@ -0,0 +1,13 @@
+package com.ll.hackathon1team;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hackathon1TeamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hackathon1TeamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/hackathon1team/Hackathon1TeamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.hackathon1team;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hackathon1TeamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
