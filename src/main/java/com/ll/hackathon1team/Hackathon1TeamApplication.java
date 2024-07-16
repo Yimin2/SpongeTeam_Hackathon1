0ba771903e6c6311e574eb9a33ba3b58f829488c
Yimin2 <gownfla14@naver.com>
2024-07-16 14:46:29 +0900

    폴더 생성 & application.yml 설정

--- src/main/java/com/ll/hackathon1team/Hackathon1TeamApplication.java
+++ src/main/java/com/ll/hackathon1team/Hackathon1TeamApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Hackathon1TeamApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Hackathon1TeamApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Hackathon1TeamApplication.class, args);
+	}
 
 }
--- src/test/java/com/ll/hackathon1team/Hackathon1TeamApplicationTests.java
+++ src/test/java/com/ll/hackathon1team/Hackathon1TeamApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Hackathon1TeamApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
