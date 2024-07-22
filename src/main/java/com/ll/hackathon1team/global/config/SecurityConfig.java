7f158821abf2b7839ad5cd518e0783befdd68cd6
Yimin2 <gownfla14@naver.com>
2024-07-22 10:27:30 +0900

    DB 연결

---
+++ src/main/java/com/ll/hackathon1team/global/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.ll.hackathon1team.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public WebSecurityCustomizer configure() {
+        return (web) -> web.ignoring()
+                .requestMatchers(toH2Console())
+                .requestMatchers("/static/**");
+    }
+}
