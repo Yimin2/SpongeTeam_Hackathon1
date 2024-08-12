9c618f0749efca08f8ec7c3f2c22a59edae5b580
Yimin2 <gownfla14@naver.com>
2024-08-12 14:04:12 +0900

    feat: basetimeentity 추가

---
+++ src/main/java/com/ll/hackathon1team/domain/reivew/entity/Review.java
@@ -0,0 +1,38 @@
+package com.ll.hackathon1team.domain.reivew.entity;
+
+import com.ll.hackathon1team.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long reviewID;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    private Long courseId;
+
+    private String reviewStatus;
+    private String courseLocation;
+    private String userMajor;
+    private String userFinish;
+    private String courseInstructor;
+    private boolean courseTeam;
+    private int reviewRate;
+    private String reviewPro;
+    private String reviewCon;
+    private String reviewProposal;
+    private boolean reviewRecommend;
+    private String reviewCertImg;
+}
